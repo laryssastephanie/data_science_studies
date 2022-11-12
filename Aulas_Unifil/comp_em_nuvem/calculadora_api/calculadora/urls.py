4ba0537d0e80edc72303b62eb96b684966b23f90
Laryssa Stephanie <laryssacosta-@hotmail.com>
2022-11-12 00:54:41 -0300

    Continuando construção da calculadora API

--- Aulas_Unifil/comp_em_nuvem/calculadora_api/calculadora/urls.py
+++ Aulas_Unifil/comp_em_nuvem/calculadora_api/calculadora/urls.py
@@ -3,7 +3,7 @@ from calculadora.views import Adicao, Subtracao, Multiplicacao, Divisao
 
 urlpatterns = [
     path('adicao/<int:a>/<int:b>/', Adicao.as_view()),
-    path('adicao/<int:a>/<int:b>/', Subtracao.as_view()),
-    path('adicao/<int:a>/<int:b>/', Multiplicacao.as_view()),
-    path('adicao/<int:a>/<int:b>/', Divisao.as_view()),
+    path('subtracao/<int:a>/<int:b>/', Subtracao.as_view()),
+    path('multiplicacao/<int:a>/<int:b>/', Multiplicacao.as_view()),
+    path('divisao/<int:a>/<int:b>/', Divisao.as_view()),
 ]
