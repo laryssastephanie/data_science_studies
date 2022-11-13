f3aadeb3b9eeb82a43126282c92094985559d1f2
Laryssa Stephanie <laryssacosta-@hotmail.com>
2022-11-12 22:07:15 -0300

    Reorganizando pastas - movendo pasta das aulas da unifil para outro repositorio

--- Aulas_Unifil/comp_em_nuvem/calculadora_api/src/calculadora/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CalculadoraConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'calculadora'
--- Aulas_Unifil/comp_em_nuvem/calculadora_api/src/calculadora/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.urls import path
-from calculadora.views import Adicao, Subtracao, Multiplicacao, Divisao
-
-urlpatterns = [
-    path('adicao/<int:a>/<int:b>/', Adicao.as_view()),
-    path('subtracao/<int:a>/<int:b>/', Subtracao.as_view()),
-    path('multiplicacao/<int:a>/<int:b>/', Multiplicacao.as_view()),
-    path('divisao/<int:a>/<int:b>/', Divisao.as_view()),
-]
